814d4f2cfbdb24dba2f1588e1cda2d2fd7488a0a
liuliang <liuliang0817@gmail.com>
2017-02-21 17:06:30 +0800

    修复类方法bug

--- lib/netease_im/chat_room.rb
+++ lib/netease_im/chat_room.rb
@@ -12,87 +12,87 @@ module NeteaseIM
       Request.send_sync('chatroom/get.action', params)
     end
     
-    def update(params)
+    def self.update(params)
       Utils.check_required_params(params, %w(roomid))
       Request.send_sync('chatroom/update.action', params)
     end
     
-    def toggle_close_stat(params)
+    def self.toggle_close_stat(params)
       Utils.check_required_params(params, %w(roomid operator valid))
       Request.send_sync('chatroom/toggleCloseStat.action', params)
     end
     
-    def set_member_role(params)
+    def self.set_member_role(params)
       Utils.check_required_params(params, %w(roomid operator target opt optvalue))
       Request.send_sync('chatroom/setMemberRole.action', params)
     end
     
-    def request_addr(params)
+    def self.request_addr(params)
       Utils.check_required_params(params, %w(roomid accid))
       Request.send_sync('chatroom/requestAddr.action', params)
     end
     
-    def send_msg(params)
+    def self.send_msg(params)
       Utils.check_required_params(params, %w(roomid msgId fromAccid msgType))
       Request.send_sync('chatroom/sendMsg.action', params)
     end
     
-    def add_robot(params)
+    def self.add_robot(params)
       Utils.check_required_params(params, %w(roomid accids))
       Request.send_sync('chatroom/addRobot.action', params)
     end
     
-    def remove_robot(params)
+    def self.remove_robot(params)
       Utils.check_required_params(params, %w(roomid accids))
       Request.send_sync('chatroom/removeRobot.action', params)
     end
     
-    def temporary_mute(params)
+    def self.temporary_mute(params)
       Utils.check_required_params(params, %w(roomid operator target muteDuration))
       Request.send_sync('chatroom/temporaryMute.action', params)
     end
     
-    def queue_offer(params)
+    def self.queue_offer(params)
       Utils.check_required_params(params, %w(roomid key value))
       Request.send_sync('chatroom/queueOffer.action', params)
     end
     
-    def queue_poll(params)
+    def self.queue_poll(params)
       Utils.check_required_params(params, %w(roomid))
       Request.send_sync('chatroom/queueOffer.action', params)
     end
     
-    def queue_list(params)
+    def self.queue_list(params)
       Utils.check_required_params(params, %w(roomid))
       Request.send_sync('chatroom/queueList.action', params)
     end
     
-    def queue_drop(params)
+    def self.queue_drop(params)
       Utils.check_required_params(params, %w(roomid))
       Request.send_sync('chatroom/queueDrop.action', params)
     end
     
-    def queue_init(params)
+    def self.queue_init(params)
       Utils.check_required_params(params, %w(roomid sizeLimit))
       Request.send_sync('chatroom/queueInit.action', params)
     end
     
-    def mute_room(params)
+    def self.mute_room(params)
       Utils.check_required_params(params, %w(roomid operator mute))
       Request.send_sync('chatroom/muteRoom.action', params)
     end
     
-    def topn(params)
+    def self.topn(params)
       Utils.check_required_params(params, %w(topn))
       Request.send_sync('chatroom/topn.action', params)
     end
     
-    def members_by_page(params)
+    def self.members_by_page(params)
       Utils.check_required_params(params, %w(roomid type endtime limit))
       Request.send_sync('chatroom/membersByPage.action', params)
     end
     
-    def query_members(params)
+    def self.query_members(params)
       Utils.check_required_params(params, %w(roomid accids))
       Request.send_sync('chatroom/queryMembers.action', params)
     end
--- lib/netease_im/user.rb
+++ lib/netease_im/user.rb
@@ -12,32 +12,32 @@ module NeteaseIM
       Request.send_sync('user/update.action', params)
     end
     
-    def refresh_token(params)
+    def self.refresh_token(params)
       Utils.check_required_params(params, %w(accid))
       Request.send_sync('user/refreshToken.action', params)
     end
     
-    def block(params)
+    def self.block(params)
       Utils.check_required_params(params, %w(accid))
       Request.send_sync('user/block.action', params)
     end
     
-    def unblock(params)
+    def self.unblock(params)
       Utils.check_required_params(params, %w(accid))
       Request.send_sync('user/unblock.action', params)
     end
     
-    def update_uinfo(params)
+    def self.update_uinfo(params)
       Utils.check_required_params(params, %w(accid))
       Request.send_sync('user/updateUinfo.action', params)
     end
     
-    def get_uinfos(params)
+    def self.get_uinfos(params)
       Utils.check_required_params(params, %w(accids))
       Request.send_sync('user/getUinfos.action', params)
     end
     
-    def set_donnop(params)
+    def self.set_donnop(params)
       Utils.check_required_params(params, %w(accid))
       Request.send_sync('user/setDonnop.action', params)
     end
